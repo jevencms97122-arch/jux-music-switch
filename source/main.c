a290559af30c689caa619acea34e4b91edcd5359
jevencms97122-arch <jeven.cms97122@gmail.com>
2026-06-09 20:16:58 +0200

    Use webSystemCreate for better CFW compat + hide URL bar

--- source/main.c
+++ source/main.c
@@ -5,11 +5,21 @@ int main(int argc, char* argv[])
     Result rc = 0;
     WebCommonConfig config;
     WebCommonReply reply;
+    const char* url = "http://188.115.125.74:3000/jux";
 
-    rc = webPageCreate(&config, "http://188.115.125.74:3000/jux");
+    // webSystemCreate uses a different applet path, more compatible with CFW
+    rc = webSystemCreate(&config, url);
+    if (R_FAILED(rc)) {
+        // fallback to standard page
+        rc = webPageCreate(&config, url);
+        if (R_SUCCEEDED(rc))
+            webConfigSetWhitelist(&config, ".*");
+    }
+
     if (R_SUCCEEDED(rc)) {
-        webConfigSetWhitelist(&config, ".*");
         webConfigSetFooter(&config, false);
+        // cast to int to hide URL bar without relying on undefined enum value
+        webConfigSetDisplayUrlKind(&config, (WebDisplayUrlKind)1);
         webConfigShow(&config, &reply);
     }
 
